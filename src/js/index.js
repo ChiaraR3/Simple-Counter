64adc99b10a1d180f2aab601796d6ab69b064051
ChiaraR3 <chiara.r.3@live.it>
2021-08-18 15:25:13 +0000

    prova finale

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-import "./icons.js";
+
 //import your own components
 function SecondsCounter(props) {
 	return (
@@ -15,12 +16,44 @@ function SecondsCounter(props) {
 			<div className="Clock">
 				<i className="far fa-clock"></i>
 			</div>
-			,<div className="OneZero">0</div>,<div className="TwoZero">0</div>,
-			<div className="ThreeZero">0</div>,<div className="FourZero">0</div>
-			,<div className="FiveZero">0</div>,<div className="Seconds">0</div>,
+			<div className="OneZero">{props.digitOne % 10}</div>
+			<div className="TwoZero">{props.digitTwo % 10}</div>
+			<div className="ThreeZero">{props.digitThree % 10}</div>
+			<div className="FourZero">{props.digitFour % 10}0</div>
+			<div className="FiveZero">{props.digitFive % 10}</div>
+			<div className="Seconds">{props.digitSeconds % 10}</div>
 		</div>
 	);
 }
+SecondsCounter.propTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitSeconds: PropTypes.number
+};
+let counter = 0;
+setInterval(function() {
+	const One = Math.floor(counter / 1000000);
+	const Two = Math.floor(counter / 100000);
+	const Three = Math.floor(counter / 10000);
+	const Four = Math.floor(counter / 1000);
+	const Five = Math.floor(counter / 100);
+	const Seconds = Math.floor(counter / 10);
+	counter++;
+	ReactDOM.render(
+		<SecondsCounter
+			digitSeconds={Seconds}
+			digitFive={Five}
+			digitFour={Four}
+			digitThree={Three}
+			digitTwo={Two}
+			digitOne={One}
+		/>,
+		document.querySelector("#app")
+	);
+	1000;
+});
 
 //render your react application
-ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
